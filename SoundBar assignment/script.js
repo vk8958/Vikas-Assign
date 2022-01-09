39e232fa0120e7c4ee1da660896c3d341a624f3b
vk8958 <vk82851781@gmail.com>
2022-01-09 17:29:21 +0530

    push

---
+++ SoundBar assignment/script.js
@@ -0,0 +1,24 @@
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"]
+
+const btnsxyz = Array.from(document.querySelectorAll(".btn"))
+
+
+function stopSounds() {
+    for (var i = 0; i < sounds.length; i++) {
+        const sound = document.getElementById(sounds[i])
+        sound.pause()
+
+        sound.currentTime = 0
+    }
+}
+function playSound(btn) {
+    btn.addEventListener("click", function () {
+        stopSounds()
+        document.getElementById(btn.innerText).play()
+    })
+}
+for (var i = 0; i < btnsxyz.length; i++) {
+    var buttonxyz = btnsxyz[i]
+
+    playSound(buttonxyz)
+}
