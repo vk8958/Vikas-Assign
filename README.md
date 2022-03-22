e4f3239cb12e272a46a4b54bc3837abe2de982ea
vk8958 <vk82851781@gmail.com>
2022-03-23 02:17:36 +0530

    push

--- README.md
+++
@@ -1,2 +0,0 @@
-# Vikas-Assign
-hii guys
---
+++ html+css Basic/script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementsByClassName('btn')
+const pera =document.getElementById('demo')
+
+
+function  txt(){
+
+    pera.innerHTML='Hi my name is Vikas Kumar'
+    
+}
