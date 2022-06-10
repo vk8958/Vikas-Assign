7128a784f899b555c64001dfba9f4b5375920d9c
vk8958 <vk82851781@gmail.com>
2022-06-10 19:21:09 +0530

    push

--- my portfolio 2/script.js
+++ my portfolio 2/script.js
@@ -1,12 +1,10 @@
-const about = document.querySelectorAll('.aboutl')
+/*const about = document.querySelectorAll('.aboutl')
 const education = document.querySelectorAll('.educationl')
 const training = document.querySelectorAll('.trainingl')
 const project = document.querySelectorAll('.projectl')
-const others = document.querySelectorAll('.othersl')
+const others = document.querySelectorAll('.othersl')*/
+ function fun(){
+    window.open('https://drive.google.com/file/d/1VMTnmmwCjSm6Qotb7StVunppmMqjuIrw/view?usp=sharing')
+}
 
-const slideLength = about.querySelectorAll('#right-section')
 
-const navItem=document.querySelectorAll('.nav-link')
-navItem.addEventListener('click', function (){
-
-})
